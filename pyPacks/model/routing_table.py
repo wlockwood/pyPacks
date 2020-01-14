db2a530945bed83009a1dde4e43a6840716e9925
William Lockwood <jinkside@gmail.com>
2020-01-14 07:55:13 -0800

    Added unit tests

--- pyPacks/model/routing_table.py
+++ pyPacks/model/routing_table.py
@@ -1,4 +1,4 @@
-from texttable import Texttable
+import unittest
 
 class RoutingTable(object):
     """
@@ -7,8 +7,11 @@ class RoutingTable(object):
     lookup() will automatically check the swapped values.
     """
 
-    # TODO: Add method descriptions. How? Docstrings?
-
+    # Looking back, this really should have been a two-dimensional array.
+    # TODO: Unit tests! ... right?
+    # TODO: Display routing table, attempt two.
+    # TODO: get_nearest_neighbor(). Takes in a location id, returns the nearest location id.
+    # TODO: Maybe route calculation should be in this class?
 
     def __init__(self, location_list):
         self.table = dict()  # Empty dictionary
@@ -24,30 +27,41 @@ class RoutingTable(object):
                     self.set_route_distance(src, dest, distance)
                     print(f"Added {src}, {dest}, {distance}")
 
-    def set_route_distance(self, id1, id2, distance):
+    def set_route_distance(self, id1: int, id2: int, distance) -> None:
+        """Add or update the distance between two places."""
         self.table[self.make_key(id1, id2)] = distance
 
-    def lookup(self, id1, id2):
+    def remove_route(self,id1: int, id2: int):
+        """Removes a route and its reverse."""
+        self.table.pop(self.make_key(id1, id2))
+        self.table.pop(self.make_key(id2, id1))
+
+    def lookup(self, id1: int, id2: int) -> int:
         print(f"Trying to find the distance between {id1} and {id2}...")
-        output = self.table.get(self.make_key(id1, id2), self.make_key(id2, id1))
+        output = self.table.get(self.make_key(id1, id2))
+        if(output == None):
+            output = self.table.get(self.make_key(id2, id1))
 
-        # table.get returning nothing for some reason returns the key that was attempted. Dunno.
-        if output == self.make_key(id1, id2) or output == self.make_key(id2, id1):
-            return None
+        if(output == None):
+            print("No route found.")
+        else:
+            print(f"Distance between {id1} and {id2}: {output}")
+
         return output
-
-    def display(self):
-        # Build headers
-        header_list = ["Name"]
-        for loc in self.locations:
-            header_list.append(loc.name[0:5])
-
-        # Build data rows
-
-
-        t_printer = Texttable()
-        t_printer.add_rows(rows=[[]])
 
     @staticmethod
     def make_key(id1, id2):
         return f"{str(id1)}-{str(id2)}"
+
+class Test_RoutingTable(unittest.TestCase):
+    def test_make_key(self):
+        valid_key = "1-2"
+        made_key = RoutingTable.make_key(1,2)
+        self.assertEqual(valid_key, made_key)
+
+    def test_set_lookup_distance(self):
+        routing = RoutingTable([])
+        routing.set_route_distance(1, 2, 10)
+        stored_dist = routing.lookup(1, 2)
+        self.assertEqual(10, stored_dist)
+
