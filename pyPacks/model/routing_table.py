11c137dfd8c6305f4e491a42d9d15a09fc046541
William Lockwood <jinkside@gmail.com>
2020-01-15 22:58:59 -0800

    Moved the unit tests. Some renames.

--- pyPacks/model/routing_table.py
+++ pyPacks/model/routing_table.py
@@ -8,13 +8,13 @@ class RoutingTable(object):
     """
 
     # Looking back, this really should have been a two-dimensional array.
-    # TODO: Unit tests! ... right?
+    # TODO: Unit tests! How to make them run?
     # TODO: Display routing table, attempt two.
     # TODO: get_nearest_neighbor(). Takes in a location id, returns the nearest location id.
     # TODO: Maybe route calculation should be in this class?
 
     def __init__(self, location_list):
-        self.table = dict()  # Empty dictionary
+        self.inner_table = dict()  # Empty dictionary
         self.locations = location_list
 
         for source_loc in self.locations:
@@ -29,18 +29,18 @@ class RoutingTable(object):
 
     def set_route_distance(self, id1: int, id2: int, distance) -> None:
         """Add or update the distance between two places."""
-        self.table[self.make_key(id1, id2)] = distance
+        self.inner_table[self.make_key(id1, id2)] = distance
 
     def remove_route(self,id1: int, id2: int):
         """Removes a route and its reverse."""
-        self.table.pop(self.make_key(id1, id2))
-        self.table.pop(self.make_key(id2, id1))
+        self.inner_table.pop(self.make_key(id1, id2), None)
+        self.inner_table.pop(self.make_key(id2, id1), None)
 
     def lookup(self, id1: int, id2: int) -> int:
         print(f"Trying to find the distance between {id1} and {id2}...")
-        output = self.table.get(self.make_key(id1, id2))
+        output = self.inner_table.get(self.make_key(id1, id2))
         if(output == None):
-            output = self.table.get(self.make_key(id2, id1))
+            output = self.inner_table.get(self.make_key(id2, id1))
 
         if(output == None):
             print("No route found.")
@@ -53,15 +53,6 @@ class RoutingTable(object):
     def make_key(id1, id2):
         return f"{str(id1)}-{str(id2)}"
 
-class Test_RoutingTable(unittest.TestCase):
-    def test_make_key(self):
-        valid_key = "1-2"
-        made_key = RoutingTable.make_key(1,2)
-        self.assertEqual(valid_key, made_key)
 
-    def test_set_lookup_distance(self):
-        routing = RoutingTable([])
-        routing.set_route_distance(1, 2, 10)
-        stored_dist = routing.lookup(1, 2)
-        self.assertEqual(10, stored_dist)
+
 
