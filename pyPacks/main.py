7536033d97b67a22b467aaba591a43e161e72bd7
William Lockwood <jinkside@gmail.com>
2020-01-23 18:05:59 -0800

    SimTime elementary functions implemented. Events trigger.

--- pyPacks/main.py
+++ pyPacks/main.py
@@ -2,21 +2,33 @@
 from data_loader import read_packages, read_locations
 from model.routing_table import RoutingTable
 from model.truck import Truck
-from model.sim_time import SimTime
+from model.sim_time import SimTime,EventAdder
 from routing_logic import RouteBuilder
 
+
 sim_time = SimTime(800, 1700)  # Global time tracker
+EventAdder.add_events(sim_time)
 
 # Initial data load
 locations = read_locations("sample_locations.csv")
 packages = read_packages("sample_packages.csv", locations, sim_time)
-
 routing_table = RoutingTable(locations)  # Build location+location distance lookup hash table
 
 # Build trucks. There's a third truck, but I think it's an error in the instructions.
 trucks = [Truck(1, sim_time), Truck(2, sim_time), Truck(3, sim_time)]
-
 route_builder = RouteBuilder(locations, packages, trucks, routing_table)
+
+while sim_time.in_business_hours():
+    events_triggered_this_minute = sim_time.increment()
+    if sim_time.get_now() % 100 == 0:
+        print("Time: ", sim_time.get_now())
+    if any(events_triggered_this_minute):
+        for e in events_triggered_this_minute:
+            print(e)
+        input("Press a key to continue")
+print("Day over!")
+exit(0)
+
 print("Truck 1")
 route_builder.determine_truckload(trucks[0])
 print("Truck 2")
--- pyPacks/model/sim_time.py
+++ pyPacks/model/sim_time.py
@@ -1,13 +1,57 @@
+from enum import Enum
+from typing import List
+
+class EventTypes(Enum):
+    TRUCK_ARRIVAL = "Truck arrived"
+    REQ_STATUS_CHECK = "Mandatory check time hit"
+    REQ_ADDRESS_CHANGE = "Mandatory address change time hit"
+    DELAYED_PACKAGES_ARRIVED = "Delayed packages have arrived"
+
+
+
+class SimEvent(object):
+    def __init__(self, event_type: EventTypes, time: float):
+        self.event_type = event_type
+        self.time = time
+
+    def __str__(self):
+        return self.event_type.value
+
+
 class SimTime(object):
     """Tracks current time in simulation.
     Floats are a immutable, and thus can't be passed by reference."""
 
     def __init__(self, initial_time: float, end_of_day: float):
         self.current = initial_time
+        self.initial_time = initial_time
         self.end_of_day = end_of_day
+        self.events: List[SimEvent] = []
 
     def get_now(self):
         return self.current
 
-    def increment(self, amount: float = 1):
-        self.current += amount
+    def get_min(self):
+        self.current//100
+
+    def increment(self):
+        self.current += 1
+        if (self.current % 100) == 60:
+            self.current += 40
+        # Triggered events
+        return [e for e in self.events if self.current == e.time]
+
+    def add_event(self, event_type: EventTypes, time: float):
+        self.events.append(SimEvent(event_type, time))
+
+    def in_business_hours(self):
+        return self.initial_time <= self.current < self.end_of_day
+
+class EventAdder(object):
+    @staticmethod
+    def add_events(sim_time: SimTime):
+        sim_time.add_event(EventTypes.REQ_STATUS_CHECK, 900)
+        sim_time.add_event(EventTypes.REQ_STATUS_CHECK, 1000)
+        sim_time.add_event(EventTypes.REQ_STATUS_CHECK, 1300)
+        sim_time.add_event(EventTypes.DELAYED_PACKAGES_ARRIVED, 905)
+        sim_time.add_event(EventTypes.REQ_ADDRESS_CHANGE, 1020)
