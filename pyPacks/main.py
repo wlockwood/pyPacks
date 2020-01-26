bebdf88c64e7a75ff3aa64e301e5019dc92c7c8b
William Lockwood <jinkside@gmail.com>
2020-01-25 21:18:59 -0800

    Improved main UI code.

--- pyPacks/main.py
+++ pyPacks/main.py
@@ -1,11 +1,21 @@
-
+from typing import List
 from data_loader import read_packages, read_locations
 from model.routing_table import RoutingTable
 from model.truck import Truck
-from model.sim_time import SimTime,EventAdder
+from model.sim_time import SimTime, EventAdder
 from routing_logic import RouteBuilder
 
 
+def prompt_with_check(prompt_string: str, input_options: List[str], allow_blank=True) -> str:
+    while True:  # Don't trust users to enter rational things.
+        user_string = input(f"{prompt_string} ({', '.join(input_options)})\n").lower()
+        if (user_string == "" and allow_blank) or user_string in input_options:
+            return user_string
+        else:
+            print(f"'{user_string}' isn't a valid option.")
+
+
+######### MAIN #########
 sim_time = SimTime(800, 1700)  # Global time tracker
 EventAdder.add_events(sim_time)
 
@@ -20,12 +30,21 @@ route_builder = RouteBuilder(locations, packages, trucks, routing_table)
 
 while sim_time.in_business_hours():
     events_triggered_this_minute = sim_time.increment()
-    if sim_time.get_now() % 100 == 0:
+    if sim_time.get_now() % 100 == 0 or any(events_triggered_this_minute):
         print("Time: ", sim_time.get_now())
     if any(events_triggered_this_minute):
         for e in events_triggered_this_minute:
             print(e)
-        input("Press a key to continue")
+        user_input = prompt_with_check("Input command:", ["load", "status", "address change"])
+        if user_input == "load":
+            print("TODO: Just load a truck? Not sure.")
+        elif user_input == "status":
+            print("TODO: write package status display code")
+        elif user_input == "address change":
+            print("TODO: write address change code")
+        else:
+            print("Continuing...")
+
 print("Day over!")
 exit(0)
 
@@ -36,15 +55,11 @@ route_builder.determine_truckload(trucks[1])
 print("Truck 3")
 route_builder.determine_truckload(trucks[2])
 
-
-
 """
 Flow:
 Build suggested delivery pattern
 Execute delivery pattern until delayed package time is hit
 """
 
-# TODO: Quick stats
-# TODO: Build main time loop? Don't use a time loop?
 # TODO: Print snapshots of current package status
 # TODO: Print snapshots of truck location
