9dff21df2e9f628de856d1387ec4d37d724eab11
William Lockwood <jinkside@gmail.com>
2020-01-26 20:05:04 -0800

    Location init doesn't choke when WGU/hub is loaded.

--- pyPacks/model/location.py
+++ pyPacks/model/location.py
@@ -1,19 +1,23 @@
 import re
 
+
 class Location(object):
-    def __init__(self, id, name, address, distances):
-        self.id = id
+    def __init__(self, location_id: int, name, address, distances):
+        self.loc_id: int = location_id
         self.name = name
 
-        # Parse address into street and zip
-        address_parts = [part.strip() for part in re.split('[\(\)]',address) if part.strip()]
-        self.address = address_parts[0]
-        self.zip = address_parts[1]
+        # Parse address into street and zip. Hub has no address.
 
+        address_parts = [part.strip() for part in re.split('[\(\)]', address) if part.strip()]
+        self.address = address_parts[0]
+        if len(address_parts) > 1:
+            self.zip = address_parts[1]
+        else:
+            self.zip = ""
         self.distances = distances
 
     def __repr__(self):
-        return f"Location({self.id},{self.name})"
+        return f"Location({self.loc_id},{self.name})"
 
     def __str__(self):
-        return f"({self.id},{self.name})"
+        return f"({self.loc_id},{self.name})"
