d72fd31d92a4d7af86a828c2446ceed0de168269
William Lockwood <jinkside@gmail.com>
2020-02-01 14:50:38 -0800

    Truck status printer added

--- pyPacks/status_printer.py
+++ pyPacks/status_printer.py
@@ -1,5 +1,6 @@
 from typing import List
 from model.package import Package, PackageStatus
+from model.truck import Truck
 
 
 def print_package_status(packages: List[Package]):
@@ -18,3 +19,9 @@ def print_package_status(packages: List[Package]):
         second_col_string = \
             "{:^10}    {:{sw}}".format(pair[1].package_id, str(pair[1].status.value), sw=status_header_len)
         print(first_col_string, "    ", second_col_string)
+
+def print_truck_status(trucks: List[Truck]):
+    for t in trucks:
+        print(f"Truck {t.truck_num} is at {t.current_location.name} and has {t.get_package_count()}: "
+              f"{', '.join(t.get_loaded_ids())}")
+        print()
